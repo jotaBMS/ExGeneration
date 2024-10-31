bdcaba29b38d81f98b8b06b5f6a6ec747645a4c7
João Bispo <joaovitor.bispo.1000@gmail.com>
2024-10-31 12:05:01 -0300

    enunciado upado

--- lacosderepeticao/LacosComFor1.java
+++ lacosderepeticao/LacosComFor1.java
@@ -1,8 +1,8 @@
-/* Escreva um algoritmo em Java, que leia 2 números inteiros via 
- * teclado, onde o primeiro número deve ser menor do que o segundo número. 
- * Caso contrário, deve ser exibida uma mensagem na tela informando que o 
- * intervalo é inválido e sair do programa. No intervalo informado, mostre 
- * na tela todes os números que são múltiplos de 3 e 5. */
+/* Escreva um algoritmo em Java, que leia 2 números inteiros via teclado, 
+onde o primeiro número deve ser menor do que o segundo número. Caso contrário, 
+deve ser exibida uma mensagem na tela informando que o intervalo é inválido e sair do programa. 
+No intervalo informado, mostre na tela todes os números que são múltiplos de 3 e 5. */ 
+
 
 package lacosderepeticao;
 
