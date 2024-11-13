a1d15e757a4d35a329bc15f69a1e4566fd6d8f47
João Bispo <joaovitor.bispo.1000@gmail.com>
2024-11-13 12:03:08 -0300

    Update Vetores2.java
    agr ta funcionando

--- vetoresematrizes/Vetores2.java
+++ vetoresematrizes/Vetores2.java
@@ -11,8 +11,8 @@ public class Vetores2 {
 		int vetorNumeros[] = new int[10];
 		int vetorPares[] = new int[5];
 		// int vetorImpares[] = new int[5];
-		int soma = 0, j = 0;
-		float media = 0;
+		int n1;
+		float media = 0, soma = 0;
 
 		for (int i = 0; i < vetorNumeros.length; i++) {
 			// inserindo os numeros no vetor (1 a 10)
@@ -20,28 +20,30 @@ public class Vetores2 {
 			vetorNumeros[i] = scanner.nextInt();
 			soma = vetorNumeros[i] + soma;
 		}
+		
+		
+		
 
+		System.out.print("Elementos pares: ");
 		for (int i = 0; i < vetorNumeros.length; i++) {
 
 			if (vetorNumeros[i] % 2 == 0) {
 				
-				for ( j = 0; j < vetorPares.length; j++) {
-				vetorPares[j] = vetorNumeros[i];
-				}
-				
+					n1 = vetorNumeros[i];
+					System.out.print(n1 + ", ");
+			
 			}
 		}
 
 		media = soma / vetorNumeros.length;
 
 		System.out.println("\n\nElementos nos índices ímpares: " + vetorNumeros[1] + " , " + vetorNumeros[3] + " , "+ vetorNumeros[5] + " , " + vetorNumeros[7] + " , " + vetorNumeros[9]);
-		
+
 		System.out.println("a soma dos elementos é igual a: " + soma);
-		//na média não esta puxando o numero depois da virgula, era pra dar "5,5" e ta dando "5" 
-		System.out.printf("a soma dos elementos é igual a: %.2f", media);
-		
-		//ta dando erro na hora de colocar os numeros no vetor de numeros pares!!!!
-		System.out.println("Elementos pares: " + vetorPares[j]); //+ " , " + vetorNumeros[2] + " , " + vetorNumeros[4]+ " , " + vetorNumeros[6] + " , " + vetorNumeros[8]);
+		// na média não esta puxando o numero depois da virgula, era pra dar "5,5" e ta
+		// dando "5"
+		System.out.printf("a média dos elementos é igual a: %.2f", media);
+
 
 		scanner.close();
 
